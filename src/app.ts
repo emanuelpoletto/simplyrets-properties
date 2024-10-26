d4705e69cd88be6d4aa01b628106232c3a89ea34
Emanuel Poletto <emanuelpoletto@users.noreply.github.com>
2024-10-26 20:12:35 -0300

    fix: update app to handle routes correctly

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,13 @@ import express from 'express';
 import { propertyRoutes } from './routes';
 
 const app = express();
+
+// Properties route
+app.use('/properties', propertyRoutes);
+
 // Index route
 app.use('/', (req, res) => {
   res.send('Visit "/properties" to load properties')
 });
-
-// Properties route
-app.use('/properties', propertyRoutes);
 
 export default app;
